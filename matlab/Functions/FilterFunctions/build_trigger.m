bea5549a8afdea5f9f6b7a704d2879a7f547a1f5
Stetmann <stetmann@googlemail.com>
2015-04-03 00:40:45 +0200

    export_filter geschrieben und get_config angepasst

---
+++ matlab/Functions/FilterFunctions/build_trigger.m
@@ -0,0 +1,21 @@
+function [ trigger ] = build_trigger( trigger_multifault )
+%BUILD_TRIGGERBUS converts the trigger_multifault to a triggerbus
+%   Detailed explanation goes here
+
+if 
+trigger.value_correlated_offset = 
+trigger.time_correlated_offset = 
+trigger.value_correlated_noise = 
+trigger.time_correlated_noise = 
+trigger.const_offset = 
+trigger.const_noise = 
+trigger.outlier = 
+trigger.spike = 
+trigger.stuck_at_zero = 
+trigger.stuck_at_x = 
+trigger.saturation = 
+trigger.const_delay = 
+trigger.time_correlated_delay = 
+
+end
+
