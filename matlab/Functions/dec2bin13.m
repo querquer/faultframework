42be68e5a6ebf3f5e2448a11f0730086722d2662
Georg Jäger <jaegergeorg@web.de>
2015-04-08 14:17:06 +0200

    added dec2bin13.m to be independent of communication Toolbox

---
+++ matlab/Functions/dec2bin13.m
@@ -0,0 +1,21 @@
+function bin = dec2bin13(dec_nr)
+    i = 1;
+    q = floor(dec_nr/2);
+    r = rem(dec_nr, 2);
+    bin(i) = r(i);
+    while 2 <= q
+        dec_nr = q;
+        i = i + 1;
+        q = floor(dec_nr/2);
+        r = rem(dec_nr, 2);
+        bin(i) = r;
+    end
+    bin(i + 1) = q;
+    bin = fliplr(bin);
+
+    sb = size(bin);
+    if(sb(1,2) < 13)
+        bin = [zeros(1,13-sb(1,2)), bin];
+    end
+end
+
