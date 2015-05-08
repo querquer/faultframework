21f7f33dca9611101d293bc40685e5fdcd557116
Maik Riestock <maikriestock@gmail.com>
2015-05-08 14:22:28 +0200

    state machine should work

--- matlab/gui_main.m
+++ matlab/gui_main.m
@@ -855,6 +855,44 @@ if(not(isempty(handles)))
     end
     assignin('base','state_machine',state);
     set(handles.state_pic, 'Tag', state_Tag);
+else
+    %% if there are no handles, get guihandles
+    handles = guihandles;
+    state_Tag = get(handles.state_pic, 'Tag');
+
+    if(isunix())
+        s0 = imread('gui/pic_state0.png');
+        s1 = imread('gui/pic_state1.png');
+        s2 = imread('gui/pic_state2.png');
+        s3 = imread('gui/pic_state3.png');
+        s4 = imread('gui/pic_state4.png');
+    else
+        s0 = imread('gui\pic_state0.png');
+        s1 = imread('gui\pic_state1.png');
+        s2 = imread('gui\pic_state2.png');
+        s3 = imread('gui\pic_state3.png');
+        s4 = imread('gui\pic_state4.png');
+    end
+
+    switch state
+        case 1
+            % process model have to be picked
+            imshow(s1,'Parent',handles.state_pic)
+            % button ausgrauen
+        case 2
+            % fault configuration have to be picked
+            imshow(s2,'Parent',handles.state_pic)
+        case 3
+            % suggest solution have to be klicked
+            imshow(s3,'Parent',handles.state_pic)
+        case 4
+            % design have to be clicked
+            imshow(s4,'Parent',handles.state_pic)
+        otherwise
+            imshow(s0,'Parent',handles.state_pic)
+    end
+    assignin('base','state_machine',state);
+    set(handles.state_pic, 'Tag', state_Tag);
 end
 
 
