d7affcb6ad89cb19daaced3f864dae1a485d7e93
Maik Riestock <maikriestock@gmail.com>
2015-05-26 12:45:39 +0200

    delete old version

---
+++ matlab/gui/gui_designing_docu.m
@@ -0,0 +1,72 @@
+%% Documentation Graphical User Interface - Testing
+% This provides a easy and clear way to create Fault Configuration files.
+% This contains the activation and parameter of all 13 fault types. The
+% activation can be adjust via the checkboxes. The parameter of the faults
+% are accessible over the button of the individual faults which will open
+% a seperate gui.
+%%
+
+%% Image of the graphical user interface
+% <<gui_faultlist.png>>
+
+
+
+%% Related Functions
+
+%%
+% * <createXML.html createXML>
+% * <myXMLwrite.html myXMLwrite>
+% * <loadFaultKonf.html loadFaultKonf>
+
+
+
+%%% Panel: Individual Fault Configuration
+
+% Elemtent: *Save Configuration*
+% Funktion: Calls uiputfile and consigns the filename to createXML()
+
+% Elemtent: *Activate*
+% Funktion: Enables/Disables the corresponding fault.
+
+% Elemtent: *Fault Name*
+% Funktion: Opens a individual gui for the corresponding fault where all
+% parameters can be edit.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
