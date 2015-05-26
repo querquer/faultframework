c01a8fb74b0e713a9c5fa31d385361b960a86783
Maik Riestock <maikriestock@gmail.com>
2015-05-26 13:48:02 +0200

    name change of the guis

--- matlab/FIDF_Framework.m
+++ matlab/FIDF_Framework.m
@@ -1,26 +1,26 @@
-function varargout = main_gui(varargin)
-% MAIN_GUI MATLAB code for main_gui.fig
-%      MAIN_GUI, by itself, creates a new MAIN_GUI or raises the existing
+function varargout = FIDF_Framework(varargin)
+% FIDF_Framwork MATLAB code for FIDF_Framwork.fig
+%      FIDF_Framwork, by itself, creates a new FIDF_Framwork or raises the existing
 %      singleton*.
 %
-%      H = MAIN_GUI returns the handle to a new MAIN_GUI or the handle to
+%      H = FIDF_Framwork returns the handle to a new FIDF_Framwork or the handle to
 %      the existing singleton*.
 %
-%      MAIN_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in MAIN_GUI.M with the given input arguments.
+%      FIDF_Framwork('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FIDF_Framwork.M with the given input arguments.
 %
-%      MAIN_GUI('Property','Value',...) creates a new MAIN_GUI or raises the
+%      FIDF_Framwork('Property','Value',...) creates a new FIDF_Framwork or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before main_gui_OpeningFcn gets called.  An
+%      applied to the GUI before FIDF_Framwork_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to main_gui_OpeningFcn via varargin.
+%      stop.  All inputs are passed to FIDF_Framwork_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help main_gui
+% Edit the above text to modify the response to help FIDF_Framwork
 
 % Last Modified by GUIDE v2.5 19-May-2015 14:24:08
 
@@ -28,8 +28,8 @@ function varargout = main_gui(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @main_gui_OpeningFcn, ...
-                   'gui_OutputFcn',  @main_gui_OutputFcn, ...
+                   'gui_OpeningFcn', @FIDF_Framwork_OpeningFcn, ...
+                   'gui_OutputFcn',  @FIDF_Framwork_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -48,26 +48,26 @@ default_settings;
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before main_gui is made visible.
-function main_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before FIDF_Framwork is made visible.
+function FIDF_Framwork_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to main_gui (see VARARGIN)
+% varargin   command line arguments to FIDF_Framwork (see VARARGIN)
 
-% Choose default command line output for main_gui
+% Choose default command line output for FIDF_Framwork
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes main_gui wait for user response (see UIRESUME)
+% UIWAIT makes FIDF_Framwork wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = main_gui_OutputFcn(hObject, eventdata, handles) 
+function varargout = FIDF_Framwork_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
