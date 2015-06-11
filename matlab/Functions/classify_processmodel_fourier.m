39b9c721c4b74f699d9c00ed4f04da094bfe0819
Stetmann <stetmann@googlemail.com>
2015-06-12 00:15:35 +0200

    fourier calssification implementiert

--- matlab/Functions/classify_processmodel_fourier.m
+++ matlab/Functions/classify_processmodel_fourier.m
@@ -2,14 +2,34 @@ function class = classify_processmodel_fourier( path_and_name, StopTime )
 
 data = getModelData(path_and_name, StopTime);
 
-assignin('base', 'Y', data);
+%assignin('base', 'Y', data);
 
 out = abs(fft(data));
 
-assignin('base', 'X', out);
+%assignin('base', 'X', out);
 
-class = 1;
+maxis = findpeaks(out);
+sMaxis = size(maxis);
 
-
+%assignin('base', 'X_maxis_A', maxis);
+i = 1;
+while(i <= sMaxis(1,2))
+   if(maxis(i) < 200)
+       maxis(i) = [];
+       sMaxis = size(maxis);
+       i = i-1;
+   end
+   i = i+1;
 end
 
+%assignin('base', 'X_size', sMaxis(1,2));
+
+if(sMaxis(1,2) <= 2)
+    class = 1;
+elseif(sMaxis(1,2) <= 4)
+    class = 2;
+else
+    class = 3;
+end
+
+end
